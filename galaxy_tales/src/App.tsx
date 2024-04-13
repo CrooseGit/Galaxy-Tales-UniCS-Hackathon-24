5a78c9b1e31449d3a8c2e19be6d4a7941908cefa
k20940rc <reuben.cruise@student.manchester.ac.uk>
2024-04-13 21:08:46 +0100

    made reccontainer modular

--- galaxy_tales/src/App.tsx
+++ galaxy_tales/src/App.tsx
@@ -1,36 +1,45 @@
-
-import './styles.css'
-import imgplace from './assets/plchldr.jpeg'
-import NavBar from './containers/NavBar'
+import './styles.css';
+import imgplace from './assets/plchldr.jpeg';
+import NavBar from './containers/NavBar';
+import RecContainer from './containers/RecContainer';
 
 function App() {
+  const getRecommendations = (article: number) => {
+    console.log(article);
+    return [
+      { title: '', content: '' },
+      { title: '', content: '' },
+      { title: '', content: '' },
+    ];
+  };
 
   return (
     <>
       <body>
         <NavBar />
         <div className='d-flex cont'>
-            <div className='article rounded m-3'>
+          <div className='article rounded m-3'>
             <img src={imgplace} className='rounded'></img>
             <h1>Placeholder Title</h1>
-            <p className='articletxt'>Lorem ipsum dolor sit amet. Et reprehenderit voluptate ut voluptatem dolores et voluptatem repellendus ea eveniet veniam id ullam placeat. Et explicabo earum et eaque rerum ad omnis autem in rerum eaque non error velit id
-             natus quos ut aliquam eligendi? Ut galisum omnis ut itaque deserunt vel voluptas iure.</p>
-             </div>
-            <div className='w-50 d-flex flex-column'>
-              <div className='reccontainer rounded my-3'>
-              <h2>Recommended 1</h2>
-              </div>
-              <div className='reccontainer rounded my-3'>
-                <h2>Recommended 2</h2>
-              </div>
-              <div className='reccontainer rounded my-3'>
-                <h2>Recommended 3</h2>
-              </div>
-            </div>
+            <p className='articletxt'>
+              Lorem ipsum dolor sit amet. Et reprehenderit voluptate ut
+              voluptatem dolores et voluptatem repellendus ea eveniet veniam id
+              ullam placeat. Et explicabo earum et eaque rerum ad omnis autem in
+              rerum eaque non error velit id natus quos ut aliquam eligendi? Ut
+              galisum omnis ut itaque deserunt vel voluptas iure.
+            </p>
           </div>
+          <div className='w-50 d-flex flex-column'>
+            {getRecommendations(0).map(
+              (article: { title: string; content: string }) => (
+                <RecContainer title={article.title} content={article.content} />
+              )
+            )}
+          </div>
+        </div>
       </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
