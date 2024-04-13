37b359d6bb2c569f0aeaa2d055c76edf63014bb7
k20940rc <reuben.cruise@student.manchester.ac.uk>
2024-04-13 23:18:03 +0100

    wip

--- galaxy_tales/src/App.tsx
+++ galaxy_tales/src/App.tsx
@@ -18,14 +18,19 @@ function App() {
       <body>
         <NavBar />
         <div className='d-flex cont'>
-        <div className='article rounded m-3 p-3'>
-              <div className='text-center'>
-                <img src={imgplace} className='rounded mb-3'></img>
-              </div>
-              <h1>Placeholder Title</h1>
-              <p className='articletxt'>Lorem ipsum dolor sit amet. Et reprehenderit voluptate ut voluptatem dolores et voluptatem repellendus ea eveniet veniam id ullam placeat. Et explicabo earum et eaque rerum ad omnis autem in rerum eaque non error velit id
-                  natus quos ut aliquam eligendi? Ut galisum omnis ut itaque deserunt vel voluptas iure.</p>
+          <div className='article rounded m-3 p-3'>
+            <div className='text-center'>
+              <img src={imgplace} className='rounded mb-3'></img>
             </div>
+            <h1>Placeholder Title</h1>
+            <p className='articletxt'>
+              Lorem ipsum dolor sit amet. Et reprehenderit voluptate ut
+              voluptatem dolores et voluptatem repellendus ea eveniet veniam id
+              ullam placeat. Et explicabo earum et eaque rerum ad omnis autem in
+              rerum eaque non error velit id natus quos ut aliquam eligendi? Ut
+              galisum omnis ut itaque deserunt vel voluptas iure.
+            </p>
+          </div>
           <div className='w-50 d-flex flex-column'>
             {getRecommendations(0).map(
               (article: { title: string; content: string }) => (
